f37e5e2027fab681fa8aa7c8c65a799b82a2dce4
MichaelKrasnovskyi <michael.krasnovskyi@gmail.com>
2025-05-06 00:30:42 +0300

    first commit

---
+++ practice/sum.js
@@ -0,0 +1,10 @@
+function sumNumbers(num1, num2) {
+
+    if (num2 === 20) {
+        return 0
+    }
+    
+    return num1 + num2;
+}
+
+module.exports = sumNumbers;
---
+++ practice/sum.test.js
@@ -0,0 +1,44 @@
+const sumNumbers = require("./sum");
+
+
+describe('Successful tests', () => {
+
+
+    beforeEach(() => {
+        console.log('Before each log');
+    })
+
+    beforeAll(() => {
+        console.log('Before all log');
+    })
+
+    afterEach(() => {
+        console.log('After each log');
+    })
+
+    afterAll(() => {
+        console.log('After all log');
+    })
+
+
+
+    test('Test sumNumbers() - 2+2=4', () => {
+        const result = sumNumbers(2, 2);
+        expect(result).toBe(4);
+    })
+
+
+    test('Test sumNumbers() - 0+10=10', () => {
+        const result = sumNumbers(0, 10);
+        expect(result).toBe(10);
+    })
+})
+
+
+describe('Failed tests', () => {
+    test('Test sumNumbers() - 10+20=30', () => {
+        const result = sumNumbers(10, 20);
+        expect(result).toBe(0);
+    })
+})
+
